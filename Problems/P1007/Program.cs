21874e36aad9b0740f6d0f007d829065c7715094
GerhardPot <gerhardjpotgieter@gmail.com>
2024-04-03 15:33:35 +0200

    Done problems 6 - 8

---
+++ Problems/P1007/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Read four integer values named A, B, C and D. Calculate and print the difference of product A and B by the product of C and D (A * B - C * D).
+
+Input
+The input file contains 4 integer values.
+
+Output
+Print DIFERENCA (DIFFERENCE in Portuguese) with all the capital letters, according to the following example, with a blank space before and after the equal signal.
+*/
+
+int A = Int32.Parse(Console.ReadLine());
+
+int B = Int32.Parse(Console.ReadLine());
+
+int C = Int32.Parse(Console.ReadLine());
+
+int D = Int32.Parse(Console.ReadLine());
+
+int DIFERENCA = (A * B) - (C * D);
+
+Console.WriteLine($"DIFERENCA = {DIFERENCA}");
