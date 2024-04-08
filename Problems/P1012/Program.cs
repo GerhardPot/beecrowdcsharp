7683816c9ecb35d4d4c24bc79d747f8854baa10c
GerhardPot <gerhardjpotgieter@gmail.com>
2024-04-08 17:59:42 +0200

    Fixed problem 12. Done problem 13.

---
+++ Problems/P1012/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+Make a program that reads three floating point values: A, B and C. Then, calculate and show:
+a) the area of the rectangled triangle that has base A and height C.
+b) the area of the radius's circle C. (pi = 3.14159)
+c) the area of the trapezium which has A and B by base, and C by height.
+d) the area of ​​the square that has side B.
+e) the area of the rectangle that has sides A and B.
+
+Input
+The input file contains three double values with one digit after the decimal point.
+
+Output
+The output file must contain 5 lines of data. Each line corresponds to one of the areas described above, always with a corresponding message (in Portuguese)
+and one space between the two points and the value. The value calculated must be presented with 3 digits after the decimal point.
+*/
+
+string abc = Console.ReadLine();
+
+int firstSpace = abc.IndexOf(" ");
+int secondSpace = abc.LastIndexOf(" ");
+
+double a = Double.Parse(abc.Substring(0, firstSpace-0));
+double b = Double.Parse(abc.Substring(firstSpace+1, secondSpace-firstSpace));
+double c = Double.Parse(abc.Substring(secondSpace+1));
+
+double triangle, circle, trapezium, square, rectangle = 0;
+
+// a
+triangle = (a / 2d) * c;
+Console.WriteLine($"TRIANGULO: {triangle.ToString("0.000")}");
+
+// b
+circle = 3.14159d * Math.Pow(c, 2);
+Console.WriteLine($"CIRCULO: {circle.ToString("0.000")}");
+
+// c
+trapezium = ((a + b) / 2d) * c;
+Console.WriteLine($"TRAPEZIO: {trapezium.ToString("0.000")}");
+
+// d
+square = Math.Pow(b, 2);
+Console.WriteLine($"QUADRADO: {square.ToString("0.000")}");
+
+// e
+rectangle = a * b;
+Console.WriteLine($"RETANGULO: {rectangle.ToString("0.000")}");
---
+++ Problems/P1013/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+The formula makes no sense. Make a simple comparison between the three integers.
+*/
+
+/*
+Make a program that reads 3 integer values and present the greatest one followed by the message "eh o maior". Use the following formula:
+Maior = (a + b + abs(a - b)) / 2
+
+Input
+The input file contains 3 integer values.
+
+Output
+Print the greatest of these three values followed by a space and the message “eh o maior”.
+*/
+
+string abs = Console.ReadLine();
+
+int firstSpace = abs.IndexOf(" ");
+int secondSpace = abs.LastIndexOf(" ");
+
+int a = Int32.Parse(abs.Substring(0, firstSpace-0));
+int b = Int32.Parse(abs.Substring(firstSpace+1, secondSpace-firstSpace));
+int s = Int32.Parse(abs.Substring(secondSpace+1));
+
+int Maior = a;
+
+if (b > Maior)
+{
+    Maior = b;
+}
+if (s > Maior)
+{
+    Maior = s;
+}
+
+Console.WriteLine($"{Maior} eh o maior");
