c2dd0c98aa869632a6ba0c6bf5e7caa6dfd86cb7
GerhardPot <gerhardjpotgieter@gmail.com>
2024-04-09 03:38:31 +0200

    Done problems 14 - 16.

---
+++ Problems/P1014/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Calculate a car's average consumption being provided the total distance traveled (in Km) and the spent fuel total (in liters).
+
+Input
+The input file contains two values: one integer value X representing the total distance (in Km) and
+the second one is a floating point number Y representing the spent fuel total, with a digit after the decimal point.
+
+Output
+Present a value that represents the average consumption of a car with 3 digits after the decimal point, followed by the message "km/l".
+*/
+
+int distance = Int32.Parse(Console.ReadLine());
+
+float spentFuel = float.Parse(Console.ReadLine());
+
+float averageConsumption = (float)distance / spentFuel;
+
+Console.WriteLine($"{averageConsumption.ToString("0.000")} km/l");
---
+++ Problems/P1015/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Read the four values corresponding to the x and y axes of two points in the plane, p1 (x1, y1) and p2 (x2, y2) and
+calculate the distance between them, showing four decimal places after the comma, according to the formula:
+
+Distance = Sqrt((x2 - x1)^2 + (y2 - y1)^2)
+
+Input
+The input file contains two lines of data. The first one contains two double values: x1 y1 and the second one also contains two double values with one digit after the decimal point: x2 y2.
+
+Output
+Calculate and print the distance value using the provided formula, with 4 digits after the decimal point.
+*/
+
+string xy1 = Console.ReadLine();
+string xy2 = Console.ReadLine();
+
+int xy1Space = xy1.IndexOf(" ");
+int xy2Space = xy2.IndexOf(" ");
+
+double x1 = double.Parse(xy1.Substring(0, xy1Space-0));
+double y1 = double.Parse(xy1.Substring(xy1Space+1));
+double x2 = double.Parse(xy2.Substring(0, xy2Space-0));
+double y2 = double.Parse(xy2.Substring(xy2Space+1));
+
+double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+Console.WriteLine(distance.ToString("0.0000"));
